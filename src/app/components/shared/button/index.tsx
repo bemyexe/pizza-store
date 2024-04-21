29e13854665475a4afd1e0c56e36c92c04bbecd8
maxim <trifonov.max@mail.ru>
2024-04-21 18:47:31 +0300

    feature(button): added button component

--- src/app/components/shared/button/index.tsx
+++ src/app/components/shared/button/index.tsx
@@ -1,5 +1,32 @@
-const Button = () => {
-  return <>button</>;
+import classnames from "classnames";
+
+import "./style.scss";
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+  title: string;
+  type?: "button" | "submit" | "reset";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  className,
+  title,
+  type = "button",
+}) => {
+  return (
+    <button
+      className={classnames("button", className)}
+      onClick={onClick}
+      title={title}
+      type={type}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
