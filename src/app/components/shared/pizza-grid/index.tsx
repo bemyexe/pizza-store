e64a2995fb7901c1a0b0c4b2899616909faa3c54
maxim <trifonov.max@mail.ru>
2024-04-25 10:45:57 +0300

    feature(service): added mockapi pizzaservice

--- src/app/components/shared/pizza-grid/index.tsx
+++ src/app/components/shared/pizza-grid/index.tsx
@@ -1,10 +1,26 @@
-import pizza from "../../../../data/pizza-variables/pizza-variables.json";
-import PizzaItem from "../pizza-item";
+import { useEffect, useState } from "react";
+import { pizzaservice } from "../../../services/pizza.service/pizza.service";
+import { PizzaItems } from "../../../services/pizza.service/response/pizza.response";
 import { v4 as uuidv4 } from "uuid";
 
 import "./style.scss";
+import PizzaItem from "../pizza-item";
 
 const PizzaGrid = () => {
+  const [pizza, setPizza] = useState<PizzaItems>([]);
+
+  async function fetchPizza() {
+    try {
+      const res = await pizzaservice.getPizzaItems();
+      setPizza(res);
+    } catch {
+      console.log("error");
+    }
+  }
+  useEffect(() => {
+    fetchPizza();
+  }, []);
+
   return (
     <div className="pizza-grid">
       {pizza.map((item) => (
