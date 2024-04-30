5b64bbedc87e5c08cc3eb7064bc21cff9574af06
maxim <trifonov.max@mail.ru>
2024-04-30 15:20:06 +0300

    add sort methods

--- src/app/components/shared/input/index.tsx
+++ src/app/components/shared/input/index.tsx
@@ -1,9 +1,18 @@
+import { FC, useContext } from "react";
 import "./style.scss";
+import { SomeContext } from "../../../pages/root-page";
 
-const Input = () => {
+const Input: FC = () => {
+  const { state, setState } = useContext(SomeContext);
+
   return (
     <>
-      <input placeholder="what pizza?" className="input" />
+      <input
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        placeholder="what pizza?"
+        className="input"
+      />
     </>
   );
 };
