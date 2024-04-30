26d33f75d5be0db03a8cd96a2c325ef6784f152f
maxim <trifonov.max@mail.ru>
2024-04-30 15:20:01 +0300

    add sort methods

--- src/app/pages/root-page/index.tsx
+++ src/app/pages/root-page/index.tsx
@@ -2,15 +2,29 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/header";
 
 import "./style.scss";
+import { createContext, useState } from "react";
+
+export type GlobalContent = {
+  state: string;
+  setState: (c: string) => void;
+};
+
+export const SomeContext = createContext<GlobalContent>({
+  state: "",
+  setState: () => {},
+});
 
 const RootPage = () => {
+  const [state, setState] = useState("");
   return (
-    <div className="background-app">
-      <div className="main-app">
-        <Header />
-        <Outlet />
+    <SomeContext.Provider value={{ state, setState }}>
+      <div className="background-app">
+        <div className="main-app">
+          <Header />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </SomeContext.Provider>
   );
 };
 
