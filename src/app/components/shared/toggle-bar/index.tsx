aceaa6df662987097a39f0c12b3150493f66e4c0
maxim <trifonov.max@mail.ru>
2024-05-03 01:24:12 +0300

    refactor(toggle-bar): added redux logic

--- src/app/components/shared/toggle-bar/index.tsx
+++ src/app/components/shared/toggle-bar/index.tsx
@@ -1,22 +1,26 @@
-import { FC, useState } from "react";
+import { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { CATEGORIES } from "../../../../data/pizza-categories";
 import Button from "../button";
 import classNames from "classnames";
 
 import "./style.scss";
+import { setCategoryId } from "../../../store/slices/filterSlice";
 
 interface ToggleBarProps {
   className?: string;
 }
 
 const ToggleBar: FC<ToggleBarProps> = ({ className }) => {
-  const [categoryId, setCategoryId] = useState(0);
+  const categoryId = useSelector((state) => state.filter.categoryId);
+  const dispatch = useDispatch();
   return (
     <div className={classNames("toggle-bar", className)}>
       {CATEGORIES.map((item, i) => (
         <Button
           key={i}
-          onClick={() => setCategoryId(i)}
+          onClick={() => dispatch(setCategoryId(i))}
           title={item}
           className={categoryId === i ? "active" : ""}
         >
