a8fef1e537e8c46d0540d3854af684b308aab1a6
maxim <trifonov.max@mail.ru>
2024-05-03 11:58:16 +0300

    feature(input): added close button

--- src/app/components/shared/input/index.tsx
+++ src/app/components/shared/input/index.tsx
@@ -6,14 +6,21 @@ const Input: FC = () => {
   const { state, setState } = useContext(SomeContext);
 
   return (
-    <>
+    <div className="search">
       <input
         value={state}
         onChange={(e) => setState(e.target.value)}
         placeholder="what pizza?"
         className="input"
       />
-    </>
+      {state && (
+        <img
+          onClick={() => setState("")}
+          className="close"
+          src="assets/close.svg"
+        />
+      )}
+    </div>
   );
 };
 
