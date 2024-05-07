e06271775c479b46c26692c60af2db82a3b54ef9
maxim <trifonov.max@mail.ru>
2024-05-07 17:40:14 +0300

    ref : move

---
+++ src/app/components/pizza-grid/index.tsx
@@ -0,0 +1,45 @@
+import { FC, useContext, useEffect, useState } from "react";
+import { pizzaservice } from "../../services/pizza.service/pizza.service";
+import { PizzaItems } from "../../services/pizza.service/response/pizza.response";
+import { v4 as uuidv4 } from "uuid";
+
+import "./style.scss";
+import PizzaItem from "../pizza-item";
+import Skeleton from "../shared/skeleton";
+import { SomeContext } from "../../pages/root-page";
+
+const PizzaGrid: FC = () => {
+  const { state } = useContext(SomeContext);
+
+  const [pizza, setPizza] = useState<PizzaItems>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPizza() {
+      try {
+        setIsLoading(true);
+        const res = await pizzaservice.getPizzaItems();
+        setPizza(res);
+      } catch {
+        console.log("error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchPizza();
+  }, []);
+
+  const skeleton = [...new Array(3)].map(() => <Skeleton key={uuidv4()} />);
+  const pizzas = pizza
+    .filter((item) => {
+      if (item.title.toLowerCase().includes(state.toLowerCase())) {
+        return true;
+      }
+      return false;
+    })
+    .map((item) => <PizzaItem key={uuidv4()} {...item} />);
+
+  return <div className="pizza-grid">{isLoading ? skeleton : pizzas}</div>;
+};
+
+export default PizzaGrid;
