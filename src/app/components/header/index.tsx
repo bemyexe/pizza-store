0027efaf1f9585b2235037a6035db9cddf0a19d9
maxim <trifonov.max@mail.ru>
2024-05-07 17:39:05 +0300

    feat(header): added redux logic

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,8 +3,11 @@ import Button from "../shared/button";
 import Input from "../shared/input";
 
 import "./styles.scss";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { totalPrice, items } = useSelector((state) => state.cart);
+  const quantity = items.reduce((sum, items) => items.quantity + sum, 0);
   return (
     <div className="header">
       <Link to={"/pizza"}>
@@ -12,8 +15,8 @@ const Header = () => {
       </Link>
       <Input />
       <Link to={"cart"}>
-        <Button onClick={() => {}} title="cart" styleType="cart">
-          CART
+        <Button onClick={() => {}} title={""} styleType="cart">
+          {totalPrice} Р. | {quantity}
         </Button>
       </Link>
     </div>
