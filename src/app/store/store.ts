3a1bef482f80ac9a09d625e17e13e62b60f548ed
maxim <trifonov.max@mail.ru>
2024-05-08 15:31:00 +0300

    feature(store): added pizza reducer

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filter from "./slices/filterSlice";
 import cart from "./slices/cartSlice";
+import pizza from "./slices/pizzaSlice";
 
 export default configureStore({
-  reducer: { filter, cart },
+  reducer: { filter, cart, pizza },
 });
