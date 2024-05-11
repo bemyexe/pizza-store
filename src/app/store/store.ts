bfd8fa36bc26be4319b520f2b1a23fb3c5687179
maxim <trifonov.max@mail.ru>
2024-05-11 23:56:31 +0300

    refactor(store): moved folders

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import filter from "./slices/filterSlice";
-import cart from "./slices/cartSlice";
-import pizza from "./slices/pizzaSlice";
+import filter from "./slices/filter/filterSlice";
+import cart from "./slices/cart/cartSlice";
+import pizza from "./slices/pizza/pizzaSlice";
 
 export default configureStore({
   reducer: { filter, cart, pizza },
