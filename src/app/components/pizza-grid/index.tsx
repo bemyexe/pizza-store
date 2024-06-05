29e1d6ce298f542d07437b72354c4edc8c6e6e1e
maxim <trifonov.max@mail.ru>
2024-06-05 15:09:40 +0300

    ref

--- src/app/components/pizza-grid/index.tsx
+++ src/app/components/pizza-grid/index.tsx
@@ -7,12 +7,14 @@ import PizzaItem from "../pizza-item";
 import Skeleton from "../shared/skeleton";
 import { SomeContext } from "../../pages/root-page";
 import { useDispatch, useSelector } from "react-redux";
-import { setItems } from "../../store/slices/pizzaSlice";
+import { setItems } from "../../store/slices/pizza/pizzaSlice";
+import { selectPizzaData } from "../../store/slices/pizza/selectors";
+import { IPizzaItem } from "../../services/pizza.service/response/pizza.response";
 
 const PizzaGrid: FC = () => {
   const { state } = useContext(SomeContext);
 
-  const pizza = useSelector((state) => state.pizza.items);
+  const pizza = useSelector(selectPizzaData);
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -33,13 +35,13 @@ const PizzaGrid: FC = () => {
 
   const skeleton = [...new Array(3)].map(() => <Skeleton key={uuidv4()} />);
   const pizzas = pizza
-    .filter((item) => {
+    .filter((item: IPizzaItem) => {
       if (item.title.toLowerCase().includes(state.toLowerCase())) {
         return true;
       }
       return false;
     })
-    .map((item) => <PizzaItem key={uuidv4()} {...item} />);
+    .map((item: IPizzaItem) => <PizzaItem key={uuidv4()} {...item} />);
 
   return <div className="pizza-grid">{isLoading ? skeleton : pizzas}</div>;
 };
